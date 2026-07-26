82dde7abc23799d6e1a1a10b339312d46ae96124
Raghav Goyal <32592021+RaghavGoyal04@users.noreply.github.com>
2026-07-26 01:54:25 +0100

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,33 @@
+class DUS:
+    #disjoint union set 
+    def __init__(self, n):
+        self.parents = {i: i for i in range(n)}
+    
+    def find_parent(self, p):
+        if p != self.parents[p]:
+            self.parents[p] = self.find_parent(self.parents[p])
+        return self.parents[p]
+
+    def has_cycle_by_union(self, p1, p2):
+        p1_parent, p2_parent = self.find_parent(p1), self.find_parent(p2)
+        #already a cycle is present thus marking it as false
+        if p1_parent == p2_parent:
+            return True
+        
+        if p1_parent < p2_parent:
+            self.parents[p2] = p1_parent
+        elif p1_parent > p2_parent:
+            self.parents[p1] = p2_parent
+
+        return False
+
+class Solution:    
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        dus = DUS(n)
+        for u, v in sorted(edges):
+            if dus.has_cycle_by_union(u, v):
+                return False
+
+        # print(f'{dus.parents=}')
+        return True if len(set(dus.parents.values())) == 1 else False
+            
